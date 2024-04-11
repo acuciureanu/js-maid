807d548adab67c875f17dffbc6b5e308be70c1aa
acuciureanu <alexandru.cuciureanu@gmail.com>
2024-04-11 22:08:15 +0300

    Fix to get compliant

--- rules/LiteralRule.ts
+++ rules/LiteralRule.ts
@@ -5,7 +5,7 @@ import type MatchingRule from "./MatchingRule";
 export default class LiteralRule implements Rule {
   apply(node: any, context: ProcessingContext, matchingRules: MatchingRule[]): ProcessingContext {
     if (node.type === "Literal" && typeof node.value === "string") {
-      matchingRules.forEach((rule) => rule.applyMatch(node.value, context));
+      matchingRules.forEach((rule) => rule.apply(node.value, context));
     }
     return context;
   }
